c6dd478d1286c8d704aa6e4b4252fbd838e45b2d
antaramol <antaramol@alum.us.es>
2023-01-25 00:33:58 +0100

    manda 1 o 0 aleatoriamente

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,6 +1,7 @@
 # publish to the mqtt topic SCF/trabajoFinal/Humdidity the message "Hello World" every 5 seconds
 import paho.mqtt.client as mqtt
 import time
+import random
 
 client = mqtt.Client()
 client.connect("test.mosquitto.org", 1883, 60)
@@ -8,6 +9,11 @@ client.connect("test.mosquitto.org", 1883, 60)
 i = 0;
 while True:
     client.publish("SCF/trabajoFinal/TempSup", str(i))
-    i = 1
+    # i = random number 1 or 0
+    i = random.randint(0, 1)
+
+
+
+    #i = 1
     print("Mensaje publicado: ", str(i))
     time.sleep(5)
