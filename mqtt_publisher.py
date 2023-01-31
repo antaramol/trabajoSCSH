e3e1af9e302d201f4b5260b0c1e6a47afd66c1ee
antaramol <antaramol@alum.us.es>
2023-01-31 23:04:49 +0100

    haciendo test

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -7,13 +7,14 @@ client = mqtt.Client()
 client.connect("test.mosquitto.org", 1883, 60)
 
 i = 0;
-while True:
-    client.publish("SCF/trabajoFinal/TempSup", str(i))
-    # i = random number 1 or 0
-    i = random.randint(0, 1)
+client.publish("SCF/trabajoFinal/conf", "20:20:20,20/10/20")
+# while True:
+#     #client.publish("SCF/trabajoFinal/TempSup", str(i))
+#     # i = random number 1 or 0
+#     i = random.randint(0, 1)
 
 
 
-    #i = 1
-    print("Mensaje publicado: ", str(i))
-    time.sleep(15)
+#     #i = 1
+#     print("Mensaje publicado: ", str(i))
+#     time.sleep(15)
