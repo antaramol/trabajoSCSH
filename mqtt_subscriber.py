efaff0872add1b3228f8abe43ff354c7ef793c63
antaramol <antaramol@alum.us.es>
2023-02-01 12:10:09 +0100

    ahora escribe en un archivo

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -1,11 +1,19 @@
 # subscribe to the mqtt topic SCF/trabajoFinal/Humdidity and print the message
 import paho.mqtt.client as mqtt
 
+# delete file data.txt if it exists
+import os
+if os.path.exists('accel_received.txt'):
+    os.remove('accel_received.txt')
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("SCF/trabajoFinal/Accel")
 
 def on_message(client, userdata, msg):
+    # append a line to the file with the msg.payload
+    with open('data.txt', 'a') as f:
+        f.write(msg.payload.decode("utf-8") + '\n')
     print(msg.topic+" "+str(msg.payload))
 
 client = mqtt.Client()
